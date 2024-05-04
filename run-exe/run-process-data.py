e01266c40be0969deb1945ffb987d307d422df92
twan507 <tuanbuianh507@gmail.com>
2024-05-04 11:20:27 +0700

    first commit

---
+++ run-exe/run-process-data.py
@@ -0,0 +1,10 @@
+import subprocess
+import os
+
+try:
+    print("Precessing data ...")
+    path = os.getcwd()
+    subprocess.run(["python", path + "\\t2m-process-data.py"], check=True)
+
+except subprocess.CalledProcessError as e:
+    print(f"Error: {type(e).__name__}")
